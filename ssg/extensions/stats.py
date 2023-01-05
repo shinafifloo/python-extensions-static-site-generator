1758ff5eb918d52ae057a1bf0b1df331c42f85e0
shinafifloo <effati.afshin@gmail.com>
2023-01-04 22:41:48 -0500

    p2

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,17 +4,17 @@ import time
 start_time = None
 total_written = 0
 
-@hooks.register("start_builder")
+@hooks.register("start_build")
 def start_build():
     global start_time
-    start_time = time.ctime
+    start_time = time.time()
 
 @hooks.register("written")
-def wtirren():
+def written():
     global total_written
     total_written += 1
 
 @hooks.register("stats")
 def stats():
-    final_time = time.ctime - start_time
+    final_time = time.time() - start_time
     average = final_time / total_written
